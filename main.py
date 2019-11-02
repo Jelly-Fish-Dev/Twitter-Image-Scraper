50cba3c0844d62c5f69d14519d4f90602105598e
Jelly_dev <isaac.g.hawkins@gmail.com>
2019-11-03 00:25:10 +1100

    added args

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from Scrape import Scrape
 from Downloader import Downloader
+import argparse
 
 
-twitter_user = "MasterDaye"
+parser = argparse.ArgumentParser("Scrape Pictures off the userpage of a Twitter User")
+parser.add_argument('-u', '--user',  metavar="user", type=str, nargs=1, help="User to parse from")
 
+args = parser.parse_args()
+#twitter_user = "MasterDaye"
+twitter_user = args.user[0]
+print(twitter_user)
 sc = Scrape("https://twitter.com/{}/media".format(twitter_user))
 dl = Downloader(sc.getImageLinks())
-dl.download("masterdaye")
+dl.download(twitter_user)
 
